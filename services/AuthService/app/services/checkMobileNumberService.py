c68831bcdc9f14cb4064e6f4c79eb742955cfa6c
GayashanGamage <gayashan.randimagamage@gmail.com>
2026-01-26 16:23:21 +0530

    improve : improve the 'check-mobile-number' end point and add radis + sms_log in to the flow

--- services/AuthService/app/services/checkMobileNumberService.py
+++ services/AuthService/app/services/checkMobileNumberService.py
@@ -1,45 +1,50 @@
-from ..db.transactions import Transactions 
-from ..util import generate_otp_code, hash_otp_code
-from datetime import datetime, timezone, timedelta
+from ..db.usesrs import userTransactions
+from ..db.sms_sender import smsSenderTransactions
+from ..db.otp_attempts import otpAttenptsTransactions
+from ..util import generate_otp_code,  send_otp_sms, create_otp_attempt_payload, log_sms_sender_payload, set_cooldown_key
 from fastapi.responses import JSONResponse
-from ..exceptions.registerExceptions import databaseUpdateFaildException
+from ..exceptions.registerExceptions import databaseUpdateFaildException, UserNotFoundException
+from app.utils.rate_limiter import get_redis_client
 
+RESEND_LIMIT = 3
+RESEND_WINDOW_SECONDS = 600
+RESEND_COOLDOWN_SECONDS = 60
 
 def checkMobileNumber(data, db):
+    """
+    check user's mobile number in the database or not, and send verification code
+    
+    :param data: mobile number
+    :param db: database connection
+    """
+    # create redis client
+    redis_client = get_redis_client()
+
     # check the mobile number on db
-    checkDb = Transactions.get_usser_detials_by_mobilenumer(data.mobile_number, db)
+    checkDb = userTransactions.get_usser_detials_by_mobilenumer(data.mobile_number, db)
     if checkDb['status'] == False:
-        return {
-            'status' : 'failed',
-            'message' : 'mobile number not found'
-        }
+        return UserNotFoundException(message="there is no user under this mobile number")
     
     # send the OPT to the mobile number
     otp_code = generate_otp_code()
-    hashOpt = hash_otp_code(otp_code)
 
     # send code to mobile number
-    # TODO: to be implemented. bellow is for temapry solution
-    print(f"Sending OTP {otp_code} to mobile number {data.mobile_number}")
+    send_id = send_otp_sms(recipient=checkDb['data'][0]['mobile_number'], otp_code=otp_code)
 
-    # store in database
-    sent_at = datetime.now(timezone.utc)
-    expires_at = sent_at + timedelta(minutes=10)
+    # store in otp attempt table
+    store_otp_attempt = otpAttenptsTransactions.create_otp_attempt(payload=create_otp_attempt_payload(user_id=checkDb['data'][0]['id'], otp_code=otp_code), db=db)
+    if store_otp_attempt['status'] == False:
+        return databaseUpdateFaildException(message="Fail to store otp attempt in db")
 
-    store = Transactions.create_otp_attempt(
-        payload={
-                "user_id": checkDb['data'][0]['id'],
-                "otp_hash": hashOpt,
-                "sent_at": sent_at.isoformat(),
-                "expires_at": expires_at.isoformat(),
-                "send_count": 1,
-                "status": "sent",
-            },
-            db=db,
-    )
-    if len(store) == 0:
-        # if return recode lenth is zero, return failed
-        return  databaseUpdateFaildException(message="Failed to store OTP attempt in database.")
+    # store in sms logs
+    store_sms_log = smsSenderTransactions.log_sms_sender(db=db, payload=log_sms_sender_payload(perpose='verification', sms_id=send_id, user_id=checkDb['data'][0]['id']))
+    if store_sms_log['status'] == False:
+        return databaseUpdateFaildException("Failed to log SMS sender. please try again later.")
+
+    # store in cache database
+    set_cache = set_cooldown_key(resend_cooldown_seconds=RESEND_COOLDOWN_SECONDS, redis_client=redis_client, id=checkDb['data'][0]['id'])
+    if set_cache == False:
+        return databaseUpdateFaildException(message="redis db not updated")
 
     # send success message
     return JSONResponse(status_code=200, content={

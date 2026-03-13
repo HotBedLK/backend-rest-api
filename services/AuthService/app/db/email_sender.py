1aef30ad54e00073602e91818338c35e2d7bd99f
GayashanGamage <gayashan.randimagamage@gmail.com>
2026-03-13 21:58:47 +0530

    remove unwanted files from db folder in Auth and reloated some functions in to 'otp_attempts' and 'users' files

--- services/AuthService/app/db/email_sender.py
+++
@@ -1,7 +0,0 @@
-from supabase import Client
-from postgrest.exceptions import APIError
-from ..exceptions.registerExceptions import SupabaseApiFailException
-
-class emaiSenderTransactions:
-
-    pass
